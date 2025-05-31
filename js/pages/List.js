d6d1f1a39c8bb16fb14eaf5a3a2f2ac718f6fd7d
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-05-31 17:08:23 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -25,7 +25,7 @@ export default {
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
-                            <p class="type-label-lg">#{{ i + 1 }}</p>
+                            <p class="type-label-lg" style="color: darkgrey;">#{{ i + 1 }}</p>
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
                             <button @click="selected = i">
