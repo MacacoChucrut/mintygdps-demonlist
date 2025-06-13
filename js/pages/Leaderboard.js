54e035b5c705aeeb89778cad8920dfe02248be16
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-06-13 18:36:14 -0400

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -112,8 +112,8 @@ export default {
                     { index: 0, color: '#FFD700' },
                     { index: 1, color: '#C0C0C0' },
                     { index: 2, color: '#CD7F32' },
-                    { index: 3, color: '#4169E1' },
-                    { index: 4, color: '#50C878' },
+                    { index: 3, color: '#4FD1C5' },
+                    { index: 4, color: '#9F7AEA' },
                 ];
 
                 for (const { index, color } of ranks) {
