edb93ba3426a25180c25063e1de7b99d2a247cc8
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-08-05 11:39:25 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -101,7 +101,7 @@ export default {
                     </template>
                     <h3>Submission Rules</h3>
                     <p>
-                        Video proof is required for Top 35 Demons. Click/Tap sounds will also be required for Top 25 Demons.
+                        Video proof is required for Top 40 Demons. Click/Tap sounds will also be required for Top 30 Demons.
                     </p>
                     <p>
                         Verifications must be uploaded in the format of a youtube video. 
