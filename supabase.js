b38ad860f60604a5b662605065751616b096be22
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-11-28 18:29:23 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const supabase = createClient(
-    "https://pikpdspddwhxjzzkhgsj.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpa3Bkc3BkZHdoeGp6emtoZ3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzNTU3NjIsImV4cCI6MjA3OTkzMTc2Mn0.GwRJ0Pk8_YYuvL7-TgIvSuOMEgZmCXQMWGWQE2yl2Tg"
-);
