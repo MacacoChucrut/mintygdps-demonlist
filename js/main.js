a66fa0a896890a47c3f1818173adc2e03eaa4137
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-06-23 16:01:06 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import routes from './routes.js';
+
+const app = Vue.createApp({});
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+});
+
+app.use(router);
+app.mount('#app');
